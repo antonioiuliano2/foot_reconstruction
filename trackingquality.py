fd90dccd10a95ed50350b964a2493730395ed13d
scanner <antonio.iuliano@unina.it>
2021-06-17 17:26:31 +0200

    adding information about last plate

--- trackingquality.py
+++ trackingquality.py
@@ -9,8 +9,21 @@ import sys
 '''launch with ipython -i trackinquality.py nsection'''
 nsection = sys.argv[1] #1,2, 3, 4,5,6,7
 
+setfile = r.TFile.Open("b000001.{}.0.0.set.root".format(nsection))
+footset = setfile.Get("set")
+
+#def getPlatebyPID(df):
+# '''getting plate by pID, according to set'''
+# return footset.GetID(int(df.PID)).ePlate
+
+lastplateset = footset.GetID(0).ePlate
+print("Opened set of section {}, last plate is {}".format(nsection,lastplateset))
+
+
 print("Processing Data Frame for section {}".format(nsection))
 df = pd.read_csv("GSI1_S{}.csv".format(nsection))
+#df["Plate"] = df.apply(getPlatebyPID,axis=1) #getting plate by pID
+
 simdf = df.query("MCTrack>=0 or TrackID>=0") #segments tracked or from the simulation
 simdf["Theta"] = np.arctan(np.sqrt(simdf["TX"] * simdf["TX"] + simdf["TY"] * simdf["TY"]))
 
@@ -22,13 +35,25 @@ trackdf = simdf.query("TrackID>=0")
 PIDlast = trackdf.groupby("TrackID").min()["PID"]
 PIDfirst = trackdf.groupby("TrackID").max()["PID"]
 npl = (PIDfirst - PIDlast) + 1
-#For each track, take the first segment, accept them if they have at least one segment Monte Carlo
+
+#For each track, take the last segment, accept them if they have at least one segment Monte Carlo
 trackdf["simulation"] = trackdf["MCEvent"]>=0 
 atleastonemc = trackdf.groupby("TrackID").any()["simulation"] #at least one segment coming from simulation
+
+#which are the most frequent MonteCarlo Event and Track for this reconstructed track?
+#mostfrequentevent = trackdf.query("MCEvent>=0").groupby(['TrackID'])['MCEvent'].agg(pd.Series.mode)
+#mostfrequenttrack = trackdf.query("MCTrack>=0").groupby(['TrackID'])['MCTrack'].agg(pd.Series.mode) #mode is not ok, returns only values present at least twice
+mostfrequentevent = trackdf.query("MCEvent>=0").groupby(['TrackID'])['MCEvent'].agg(lambda x:x.value_counts().index[0])
+mostfrequenttrack = trackdf.query("MCEvent>=0").groupby(['TrackID'])['MCTrack'].agg(lambda x:x.value_counts().index[0])
+
 trackdf = trackdf.groupby("TrackID").last()
+
+#adding computed information to our dataset
 trackdf["nseg"] = nseg
 trackdf["npl"] = npl
 trackdf["fedraeff"] = nseg/npl
+trackdf["MostCommonMCEvent"] = mostfrequentevent
+trackdf["MostCommonMCTrack"] = mostfrequenttrack
 
 trackdf = trackdf[atleastonemc]
 
@@ -37,10 +62,38 @@ maxnseg = int(np.max(nseg))
 simdf = simdf.query("MCEvent>=0")
 nsegtrue = simdf.groupby(["MCEvent","MCTrack"]).count()["ID"] #true length of the Monte Carlo track
 maxnsegtrue = int(np.max(nsegtrue))
-#merging this information in trackID
 
-trackdf = trackdf.reset_index().merge(nsegtrue.reset_index(), how = "left", on = ["MCEvent","MCTrack"])
-trackdf = trackdf.reset_index().merge(nsegsamemc.reset_index(), how = "left", on = ["TrackID","MCEvent","MCTrack"])
+#let us consider now signal segments which have been tracked
+simtrackdf = simdf.query("TrackID>=0")
+#grouping by MCEvent and MCTrack, we can get where the track ends (even counting splitting in different reco tracks)
+simtrackdf = simtrackdf.sort_values("PID")
+lastplateMCTrack = simtrackdf.groupby(["MCEvent","MCTrack"]).first()["PID"]
+
+#merging the MCTrue information in trackID, according to most common MCEvent and MCTrack
+nsegtrue = nsegtrue.reset_index()
+nsegsamemc = nsegsamemc.reset_index()
+lastplateMCTrack = lastplateMCTrack.reset_index()
+
+nsegtrue["MostCommonMCEvent"] = nsegtrue["MCEvent"]
+nsegtrue["MostCommonMCTrack"] = nsegtrue["MCTrack"]
+
+del nsegtrue["MCEvent"]
+del nsegtrue["MCTrack"]
+
+nsegsamemc["MostCommonMCEvent"] = nsegsamemc["MCEvent"]
+nsegsamemc["MostCommonMCTrack"] = nsegsamemc["MCTrack"]
+
+del nsegsamemc["MCEvent"]
+del nsegsamemc["MCTrack"]
+
+lastplateMCTrack["MostCommonMCEvent"] = lastplateMCTrack["MCEvent"]
+lastplateMCTrack["MostCommonMCTrack"] = lastplateMCTrack["MCTrack"]
+
+del lastplateMCTrack["MCEvent"]
+del lastplateMCTrack["MCTrack"]
+
+trackdf = trackdf.reset_index().merge(nsegtrue, how = "left", on = ["MostCommonMCEvent","MostCommonMCTrack"])
+trackdf = trackdf.reset_index().merge(nsegsamemc, how = "left", on = ["TrackID","MostCommonMCEvent","MostCommonMCTrack"])
 
 #renaming labels
 trackdf["PID"] = trackdf["PID_x"]
@@ -53,13 +106,17 @@ del trackdf["ID_x"]
 del trackdf["PID_y"]
 del trackdf["PID_x"]
 
+trackdf = trackdf.reset_index().merge(lastplateMCTrack, how = "left", on = ["MostCommonMCEvent","MostCommonMCTrack"])
+trackdf["PID"] = trackdf["PID_x"]
+trackdf["lastplateMCTrack"] = trackdf["PID_y"]
+
 print("end preparation of dataframe, making plots")
 
 #computing efficiency
 trackdf["efficiency"] = trackdf["nsegsamemc"]/trackdf["nsegtrue"]
 
 #how many reco tracks for each true track?
-nsplit = trackdf.groupby(["MCEvent","MCTrack"]).count()["ID"]
+nsplit = trackdf.groupby(["MostCommonMCEvent","MostCommonMCTrack"]).count()["ID"]
 
 #making histograms, drawing them
 hsplit = r.TH1I("hsplit","number of reconstructed tracks for each true track;Nsplit",10,1,10)
