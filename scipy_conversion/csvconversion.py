f88ee266ed1a93258fd2f23f1bad87025c319f7b
scanner <antonio.iuliano@unina.it>
2021-10-15 14:01:28 +0200

    storing only particles from simulated signal, no background

--- scipy_conversion/csvconversion.py
+++ scipy_conversion/csvconversion.py
@@ -18,17 +18,17 @@ os.system("ln -s b000001.{}.0.0.set.root b000001.0.0.0.set.root".format(nsection
 def applyconversion(nbrick,section):
  '''convert couples ROOT files into a csv'''
 
- df = feutils.builddataframe(nbrick)
+ df = feutils.builddataframe(nbrick,cutstring="s.eMCEvt>=0", footsim=True)
  #df = feutils.addtrackindex(df,"b00000{}.{}.0.0.trk.root".format(nbrick,nsection))
 
  return df 
 
 #the two steps can now be done together, without an intermediate file
-NGSI = 2
+NGSI = 1
 df = applyconversion(NGSI,0)
 
 #df = df.drop(columns = ["P","Flag"])
 #simfile = r.TFile.Open(sys.argv[2])
 #df = desy19.addtrueMCinfo(df,simfile, True)
 #df.to_csv('GSI1_S{}.csv'.format(nsection),index=False)
-df.to_csv('GSI{}.csv'.format(NGSI),index=False)
+df.to_csv('GSI{}_simonly.csv'.format(NGSI),index=False)
